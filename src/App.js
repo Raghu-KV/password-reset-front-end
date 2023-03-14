67434ec6d6d2e0980d59d935e27106a055726f9c
Raghu-KV <raghunandanv19@gmail.com>
2023-03-14 21:41:43 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-slate-800">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/log-in" element={<Login />} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignUp() {
+  return <div>SignIn</div>;
+}
+
+export default SignUp;
